8c429b1d67d387575716c21db3a180161e81984e
Mario Lucassen <Mario.lucassen@hotmail.com>
2022-07-01 01:09:15 +0200

    Added a ton of winget installs and updated readme

--- README.md
+++ README.md
@@ -8,11 +8,56 @@ Applications to figure out how to install
 - [x] Windows Package Manager (winget) [Installed by default](https://docs.microsoft.com/en-us/windows/package-manager/winget/) <br>
 - [x] Git <br>
 - [x] Vim <br>
-- [ ] Gource <br>
-- [ ] VSCode <br>
-- [ ] CORSAIR iCUE 4 software <br>
-- [ ] OpenSSL <br>
-- [ ] HWINFO64 <br>
+- [x] Gource <br>
+- [x] VSCode <br>
+- [x] CORSAIR iCUE 4 software <br>
+- [x] HWINFO64 <br>
+- [x] Stellarium <br>
+- [x] balenaEtcher <br>
+- [x] CMake <br>
+- [x] VisualStudio2022 <br>
+- [x] SpeedCrunch <br>
+- [x] RuneLite <br>
+- [x] ScreenToGif <br>
+- [x] WizTree <br>
+- [x] Docker <br>
+- [x] Minikube | Kubernetes & Lens <br>
+- [x] PowerToys <br>
+- [x] Raindrop.io <br>
+- [x] OSU! <br>
+- [x] Whatsapp <br>
+- [x] Spotify <br>
+- [x] Steam <br>
+- [x] Origin (EA app) <br>
+- [x] Discord <br>
+- [x] Google Chrome <br>
+- [x] Edge (installed by default) <br>
+- [x] Firefox <br>
+- [x] Borderless Gaming <br>
+- [x] iTunes <br>
+- [x] AltServer <br>
+- [x] OneDrive (installed by default) <br>
+- [x] Epic Games Launcher <br>
+- [x] MiKTeX <br>
+- [x] Nodejs <br>
+- [x] NVIDIA GeForce & CUDA DRIVERS etc <br>
+- [x] OBS Studio <br>
+- [x] Postman <br>
+- [x] WinSCP <br>
+- [x] Python (newest version?) <br>
+- [x] qBittorrent <br>
+- [x] TeamSpeak 3 <br>
+- [x] Wacom Tablet drivers <br>
+- [x] VLC media player <br>
+- [x] WinZip <br>
+- [x] NordVPN <br>
+- [x] Microsoft office <br>
+- [x] Plex <br>
+- [x] mGBA <br>
+- [x] ActivityWatch <br>
+- [x] Netflix <br>
+- [x] MusicBee & LastFm scrobbling <br>
+- [x] Teams <br>
 - [ ] JAVA 11 & 17 <br>
 - [ ] PixInsight <br>
 - [ ] PIPP <br>
@@ -20,62 +65,18 @@ Applications to figure out how to install
 - [ ] DeepSkyStacker <br>
 - [ ] SharpCap <br>
 - [ ] AutoStakkert <br>
-- [ ] Stellarium <br>
-- [ ] FileZilla <br>
-- [ ] balenaEtcher <br>
-- [ ] CMake <br>
-- [ ] VisualStudio2022 <br>
-- [ ] MusicBee & LastFm scrobbling <br>
 - [ ] Audient (creative app) <br>
-- [ ] SpeedCrunch <br>
 - [ ] RuneScape <br>
 - [ ] Alt1 Toolkit <br>
-- [ ] RuneLite <br>
-- [ ] ScreenToGif <br>
 - [ ] MultiMC Launcher <br>
-- [ ] NordVPN <br>
 - [ ] Telescopius <br>
-- [ ] WizTree <br>
-- [ ] Docker? <br>
-- [ ] Minikube | Kubernetes & Lens? <br>
 - [ ] MSSQL server? <br>
-- [ ] PowerToys <br>
-- [ ] Raindrop.io <br>
-- [ ] OSU! <br>
-- [ ] Microsoft office <br>
-- [ ] Whatsapp <br>
-- [ ] Spotify <br>
 - [ ] Vegas Pro 14 <br>
 - [ ] Battle.net <br>
-- [ ] Steam <br>
-- [ ] Origin <br>
-- [ ] Teams <br>
-- [ ] Discord <br>
-- [ ] Google Chrome <br>
-- [ ] Edge <br>
-- [ ] Firefox <br>
-- [ ] Borderless Gaming <br>
-- [ ] AltServer? <br>
 - [ ] iCloud & Photos <br>
-- [ ] iTunes <br>
 - [ ] Adobe Lightroom & Photoshop 2022 <br>
-- [ ] OneDrive <br>
-- [ ] Epic Games Launcher <br>
-- [ ] MiKTeX <br>
-- [ ] Netflix <br>
-- [ ] Nodejs <br>
-- [ ] NVIDIA GeForce & CUDA DRIVERS etc <br>
-- [ ] OBS Studio <br>
-- [ ] Postman <br>
-- [ ] PuTTY or WinSCP <br>
-- [ ] Python (newest version?) <br>
-- [ ] qBittorerent <br>
 - [ ] Rust <br>
-- [ ] TeamSpeak 3 <br>
-- [ ] Wacom Tablet drivers <br>
-- [ ] VLC media player <br>
 - [ ] Wallpaper Engine <br>
-- [ ] WinZip <br>
 - [ ] ffmpeg <br>
 - [ ] MoveToDesktop <br>
 
--- post-bootstrap.ps1
+++ post-bootstrap.ps1
@@ -25,10 +25,76 @@ function Dotfile-WinGet-Apps()
   Append-EnvPathIfExists "C:\Program Files\Git\bin\" 'User'
 
   winget install -e --id vim.vim -v 9.0.0009
-  # Robocopy "C:\Program Files\Vim\vim90" "C:\bin\vim" *vim.exe /s /NFL /NDL /NJH /NJS /nc /ns /np
-  # New-Item -Path "C:\bin\vim\" -ItemType Directory -Force
-  # cmd /c mklink "C:\bin\vim\vim.exe" "C:\Program Files\Vim\vim90\vim.exe"
   Append-EnvPathIfExists "C:\Program Files\Vim\vim90\" 'User'
+
+  # TODO: test these installs..
+  winget install -e --id RuneLite.RuneLite # launcher
+  winget install -e --id NickeManarin.ScreenToGif
+  winget install -e --id Valve.Steam
+  winget install -e --id OpenJS.NodeJS
+  winget install -e --id Discord.Discord
+  winget install -e --id Docker.DockerDesktop
+  winget install -e --id Kubernetes.minikube
+  winget install -e --id Mirantis.Lens
+  winget install -e --id Microsoft.VisualStudioCode
+  # vscode preferences?
+  winget install -e --id Microsoft.VisualStudio.2022.Community
+  # vsstudio preferences?
+  winget install -e --id Nvidia.GeForceExperience
+  winget install -e --id acaudwell.Gource
+  winget install -e --id Corsair.iCUE.4
+  # icue profile?
+  winget install -e --id REALiX.HWiNFO
+  winget install -e --id WinSCP.WinSCP
+  winget install -e --id Balena.Etcher
+  winget install -e --id Stellarium.Stellarium
+  winget install -e --id Kitware.CMake
+  # cmake special configs?
+  winget install -e --id SpeedCrunch.SpeedCrunch
+  winget install -e --id AntibodySoftware.WizTree
+  winget install -e --id Microsoft.PowerToys
+  # powertoys fancyzones settings
+  winget install -e --id RustemMussabekov.Raindrop
+  winget install -e --id Peppy.Osu!
+  # for osu download songs from NAS?
+  winget install -e --id WhatsApp.WhatsApp
+  winget install -e --id Spotify.Spotify
+  winget install -e --id ElectronicArts.EADesktop
+  # EA app or origin?
+  winget install -e --id Google.Chrome
+  winget install -e --id Mozilla.Firefox
+  winget install -e --id Codeusa.BorderlessGaming
+  winget install -e --id Apple.iTunes
+  winget install -e --id RileyTestut.AltServer
+  winget install -e --id EpicGames.EpicGamesLauncher
+  winget install -e --id ChristianSchenk.MiKTeX
+  winget install -e --id Nvidia.GeForceExperience
+  winget install -e --id Nvidia.CUDA
+  winget install -e --id OBSProject.OBSStudio
+  winget install -e --id Postman.Postman
+  winget install -e --id WinSCP.WinSCP
+  winget install -e --id Python.Python.3
+  winget install -e --id qBittorrent.qBittorrent
+  winget install -e --id TeamSpeakSystems.TeamSpeakClient
+  winget install -e --id Wacom.WacomTabletDriver
+  winget install -e --id VideoLAN.VLC
+  winget install -e --id Corel.WinZip
+  winget install -e --id 9P4CLT2RJ1RS --accept-package-agreements
+  # musicbee; still need to install last.fm scrobbling
+  winget install -e --id NordVPN.NordVPN
+  winget install -e --id Microsoft.Office
+  winget install -e --id Microsoft.Teams 
+  winget install -e --id 9WZDNCRFJ3TJ --accept-package-agreements
+  # 9WZDNCRFJ3TJ netflix (app allows for 4k playback, which is not achievable in browsers)
+  winget install -e --id Plex.Plex
+  winget install -e --id ActivityWatch.ActivityWatch
+  winget install -e --id JeffreyPfau.mGBA
+  
+  # NOTE: not for now..
+  # winget install -e --id Plex.Plexamp 
+  # winget install -e --id JetBrains.Toolbox 
+  # winget install -e --id Anaconda.Anaconda3 
+  # winget install -e --id Rainmeter.Rainmeter 
 }
 
 function Dotfiles-Post-Bootstrap-Install() 
